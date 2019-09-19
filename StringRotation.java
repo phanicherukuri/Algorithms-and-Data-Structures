78481c2723a5f1a75c36312b379eb57f2423e173
Phani Cherukuri <cherukuriphanichowdary@gmail.com>
2019-09-18 22:41:39 -0700

    CTCI questions

--- StringRotation.java
+++ StringRotation.java
@@ -1,5 +1,11 @@
 package crackingthecodinginterview;
 
+/*
+ *  Assume you have a method i 5Su b 5 tr ing which checks if one word is a substring
+of another. Given two strings, 51 and 52, write code to check if 52 is a rotation of 51 using only one
+call to i5Sub5tring (e.g., "waterbottle" is a rotation of"erbottlewat"). 
+ */
+
 public class StringRotation {
 
 	public static void main(String[] args) {
